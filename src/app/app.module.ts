86e0d19d33d6d1cfdbcc81d75ac082d409acaf6d
assefamaru <me@alexandermaru.com>
2017-08-03 16:32:45 -0400

    Add 404, page not found component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule }     from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
+import { PageNotFoundComponent } from './views/404/not-found.component';
 
 @NgModule({
     declarations: [
         AppComponent,
-        HomeComponent
+        HomeComponent,
+        PageNotFoundComponent
     ],
     imports: [
         BrowserModule,
