243c44b97fade4a3e9dc9f6cfd8f4096ebe9d801
assefamaru <me@alexandermaru.com>
2017-08-07 18:41:36 -0400

    Add site header - Add material components + icons - Add fontawesome icons - Make header size portable

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-import { AppRoutingModule }     from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from '@angular/material';
+import 'hammerjs';
+//
+// import { MaterialModule } from './material.module';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './views/home/home.component';
@@ -19,6 +23,8 @@ import { FooterComponent } from './partials/footer/footer.component';
     ],
     imports: [
         BrowserModule,
+        BrowserAnimationsModule,
+        MaterialModule,
         AppRoutingModule
     ],
     providers: [],
