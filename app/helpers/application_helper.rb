09d070fcad01ac82d24d2bf551b86ff67451afce
assefamaru <me@alexandermaru.com>
2018-03-04 16:18:34 -0500

    Update site meta+content

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+    # Returns the full title of a page.
+    def full_title(page_title = '')
+		base_title = "Hamere Noah Kidane Mihret EOTC"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " - " + base_title
+		end
+	end
+
+    # Strips all whitespace between the
+    # HTML tags in the passed block.
+    def spaceless(&block)
+		contents = capture(&block)
+		contents.strip.gsub(/>\s+</, '><').html_safe
+	end
 end
