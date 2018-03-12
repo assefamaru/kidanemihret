28269fb5b31aca4d10f1ba4ccd3ceb4cef161bea
assefamaru <me@alexandermaru.com>
2018-03-12 02:19:06 -0400

    Create site header

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -2,6 +2,15 @@ class StaticController < ApplicationController
     def home
     end
 
+    def about
+    end
+
+    def contact
+    end
+
+    def coming
+    end
+
     def temp
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
-    root 'static#temp'
+    root 'static#home'
+    get  '/about',           to: 'static#about'
+    get  '/contact',         to: 'static#contact'
+    get  '/coming-soon',     to: 'static#coming'
+    get  '/temp',            to: 'static#temp'
 end
