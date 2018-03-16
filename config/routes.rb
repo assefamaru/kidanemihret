62b1adc8a6cb6aeaf1ade2be019efca13bf307e7
assefamaru <me@alexandermaru.com>
2018-03-16 00:47:03 -0400

    Refactor header

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
     root 'static#home'
+    
     get  '/about',           to: 'static#about'
     get  '/contact',         to: 'static#contact'
+
     get  '/coming-soon',     to: 'static#coming'
     get  '/temp',            to: 'static#temp'
 end
