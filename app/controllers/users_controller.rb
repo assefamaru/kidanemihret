db8e4acdb7fb23da12c4bf3ab166d8da43dca2e5
assefamaru <me@alexandermaru.com>
2018-10-03 12:09:06 -0400

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,6 +10,7 @@ class UsersController < ApplicationController
     def create
         @user = User.new(user_params)
         if @user.save
+            log_in @user
             flash[:success] = "Welcome to Kidane Mihret EOTC #{@user.name.partition(" ").first}!"
             redirect_to @user
         else
