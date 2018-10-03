20f176c720518a6fbd70fcf71fc71a7e89ee1db4
assefamaru <me@alexandermaru.com>
2018-10-03 11:34:50 -0400

    Design signup logic and handle validation errors. Force SSL on production

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,7 @@
 module ApplicationHelper
     # Return full title of a page.
     def full_title(page_title = '')
-        base_title = "Kidane Mihret EOTC."
+        base_title = "Kidane Mihret EOTC"
         if page_title.empty?
             "Kidane Mihret ― Ethiopian Orthodox Tewahedo Church."
         else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  root 'static#home'
-  get  'about',   to: 'static#about'
-  get  'contact', to: 'static#contact'
-  get  'signup',  to: 'users#new'
+    root      'static#home'
+    get       'about',         to: 'static#about'
+    get       'contact',       to: 'static#contact'
+    get       'signup',        to: 'users#new'
+    post      '/signup',       to: 'users#create'
+    resources :users
 end
