7307ffb7fb1139a22d11b8c50451739f6cc5b14d
assefamaru <me@alexandermaru.com>
2018-12-13 12:27:56 -0500

    Finish layout and routes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+    # Return full title of a page.
+    def full_title(page_title = '')
+        base_title = "Kidane Mihret EOTC"
+        if page_title.empty?
+            "Kidane Mihret ― Ethiopian Orthodox Tewahedo Church."
+        else
+            page_title + " ― " + base_title
+        end
+    end
+
+    # Strip all whitespace between HTML tags in passed block.
+    def spaceless(&block)
+        contents = capture(&block)
+        contents.strip.gsub(/>\s+</, '><').html_safe
+    end
 end
