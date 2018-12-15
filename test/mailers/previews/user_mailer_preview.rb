5b9c4056e5c4ac7c1033593f70e664ed40ef14ed
assefamaru <me@alexandermaru.com>
2018-12-15 06:02:38 -0500

    Add password reset

--- test/mailers/previews/user_mailer_preview.rb
+++ test/mailers/previews/user_mailer_preview.rb
@@ -8,9 +8,10 @@ class UserMailerPreview < ActionMailer::Preview
         UserMailer.account_activation(user)
     end
 
-  # Preview this email at http://localhost:8080/rails/mailers/user_mailer/password_reset
-  def password_reset
-    UserMailer.password_reset
-  end
-
+    # Preview this email at http://localhost:8080/rails/mailers/user_mailer/password_reset
+    def password_reset
+        user = User.first
+        user.reset_token = User.new_token
+        UserMailer.password_reset(user)
+    end
 end
