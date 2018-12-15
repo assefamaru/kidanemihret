e78979dcbaacd82bb6c4371324952beddfa6f4b4
assefamaru <me@alexandermaru.com>
2018-12-15 04:25:41 -0500

    Finish user edit, update, index, and destroy actions

--- Gemfile
+++ Gemfile
@@ -64,3 +64,4 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 gem 'bootstrap', '~> 4.1.3'
 gem 'jquery-rails'
 gem 'rails-controller-testing'
+gem 'faker', '1.7.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create!(first_name:  "Alexander",
+             last_name: "Maru",
+             email: "i@alexandermaru.com",
+             password:              "12345678",
+             password_confirmation: "12345678",
+             admin: true)
+
+99.times do |n|
+    name = Faker::Name.name
+    first_name  = name.partition(" ").first
+    last_name = name.partition(" ").last
+    email = "example-#{n+1}@railstutorial.org"
+    password = "password"
+    User.create!(first_name:  first_name,
+                 last_name: last_name,
+                 email: email,
+                 password:              password,
+                 password_confirmation: password)
+end
