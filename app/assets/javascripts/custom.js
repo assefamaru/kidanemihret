11237913a91071dfaa36abeca760bb2b833d7b92
Alexander Maru <i@alexandermaru.com>
2019-10-23 12:22:36 -0400

    Minor header refactoring for better style

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,23 +1,13 @@
-function closeSidebar() {
-  var element, posname, negname, arr;
+function openSidebar() {
+  var element, name;
   element = document.getElementById("sidebar");
-  posname = "sidebar-closed";
-  arr = element.className.split(" ");
-  if (arr.indexOf(posname) == -1) {
-    element.className += " " + posname;
-  }
-  negname = "sidebar-open"
-  element.className = element.className.replace(/\bnegname\b/g, "");
+  name = "sidebar-open";
+  element.className += " " + name;
 }
 
-function openSidebar() {
-  var element, posname, negname, arr;
+function closeSidebar() {
+  var element, name;
   element = document.getElementById("sidebar");
-  posname = "sidebar-open";
-  arr = element.className.split(" ");
-  if (arr.indexOf(posname) == -1) {
-    element.className += " " + posname;
-  }
-  negname = "sidebar-closed"
-  element.className = element.className.replace(/\bnegname\b/g, "");
+  name = "sidebar-open"
+  element.className = element.className.replace(/\bname\b/g, "");
 }
