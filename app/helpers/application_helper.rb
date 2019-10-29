63cabb6608134bcccd2f03c470886eb0472af66f
Alexander Maru <i@alexandermaru.com>
2019-10-29 01:07:36 -0400

    Upgrade app to rails 6.0.0 with webpack, and a react front-end

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,16 +1,2 @@
 module ApplicationHelper
-  # Return full title of a page
-  def full_title(page_title = '')
-    if page_title.empty?
-      "Kidane Mihret — Ethiopian Orthodox Tewahedo Church."
-    else
-      page_title + " — Kidane Mihret EOTC"
-    end
-  end
-
-  # Strip all whitespace between HTML tags in passed block
-  def spaceless(&block)
-    contents = capture(&block)
-    contents.strip.gsub(/>\s+</, '><').html_safe
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,3 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-
-  get    'about',               to: 'static_pages#about'
-  get    'contact',             to: 'static_pages#contact'
-  get    'bible',               to: 'static_pages#bible'
-  get    'calendar',            to: 'static_pages#calendar'
-  get    'donate',              to: 'static_pages#donate'
-  get    'faq',                 to: 'static_pages#faq'
-
-  get    '/signup',             to: 'users#new'
-  post   '/signup',             to: 'users#create'
-  get    '/login',              to: 'sessions#new'
-  post   '/login',              to: 'sessions#create'
-  delete '/logout',             to: 'sessions#destroy'
-
-
-  resources :users
-  resources :account_activations, only: [:edit]
-  resources :password_resets,     only: [:new, :create, :edit, :update]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
