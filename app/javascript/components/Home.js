b5f3edb7f3e18ce13e5d7a104a823c198ec4c930
Alexander Maru <i@alexandermaru.com>
2019-12-25 01:41:16 -0500

    Update home

--- app/javascript/components/Home.js
+++ app/javascript/components/Home.js
@@ -14,6 +14,33 @@ const heroStyles = makeStyles(theme => ({
   },
 }));
 
+function Old(props) {
+
+  return (
+    <Container maxWidth="md">
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={4} md={4}>
+          <div>what</div>
+        </Grid>
+        <Grid item xs={12} sm={8} md={8}>
+          <Typography variant="h1">
+            Kidane Mihret
+          </Typography>
+          <Typography variant="h6">
+            Hamere Noah Kidane Mihret Ethiopian Orthodox Tewahedo Church serves the Orthodox Christian community of Kitchener-Waterloo in Ontario, Canada.
+          </Typography>
+          <Typography variant="h6">
+            This website provides information about the essential services the church provides to the community. It also contains great resources about our faith and Ethiopian tradition.
+          </Typography>
+          <Typography variant="h6">
+            We welcome you in worship in the name of the Father, the Son, and the Holy Spirit, One God, Amen.
+          </Typography>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+}
+
 function Hero(props) {
   const classes = heroStyles();
 
