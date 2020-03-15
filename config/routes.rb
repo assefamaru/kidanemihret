2e870efb645202bffbb769ef6a72bc384a1cee1a
Alexander Maru <i@alexandermaru.com>
2020-03-15 02:16:44 -0400

    Restart

--- config/routes.rb
+++
@@ -1,8 +0,0 @@
-Rails.application.routes.draw do
-  root 'static_pages#home'
-  get  'about',   to: 'static_pages#about'
-  get  'contact', to: 'static_pages#contact'
-  
-  get  '/store',  to: "home#index"
-  mount ShopifyApp::Engine, at: '/store'
-end
