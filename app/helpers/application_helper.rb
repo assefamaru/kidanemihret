d272aa389fb79a70c571430caf2d5cc527d5735a
Alexander Maru <i@alexandermaru.com>
2020-03-17 11:16:40 -0400

    Update site head metas

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  # Return full title of a page
+  def full_title(page_title = '')
+    if page_title.empty?
+      "Hamere Noah Kidane Mihret EOTC"
+    else
+      page_title + " — Kidane Mihret EOTC"
+    end
+  end
+
+  # Strip all whitespace between HTML tags in passed block
+  def spaceless(&block)
+    contents = capture(&block)
+    contents.strip.gsub(/>\s+</, '><').html_safe
+  end
 end
