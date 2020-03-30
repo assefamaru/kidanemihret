60a00c430fd78dfee7e51504d65976a9bbde95ed
Alexander Maru <i@alexandermaru.com>
2020-03-30 03:48:47 -0400

    Fix routing bug when creating sessions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,24 +3,19 @@ class SessionsController < ApplicationController
   end
 
   def create
-    if ! User.any?
-      flash[:error] = "Sign up before attempting to log in."
-      redirect_to login_path
-    else
-      user = User.find_by(email: params[:session][:email].downcase)
-      if user && user.authenticate(params[:session][:password])
-        if user.activated?
-          log_in user
-          params[:session][:remember_me] == '1' ? remember(user) : forget(user)
-          redirect_back_or user
-        else
-          flash[:warning] = "You need to activate your account before you can log in. Check your email for the activation link."
-          redirect_to login_path
-        end
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      if user.activated?
+        log_in user
+        params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+        redirect_to user
       else
-        flash.now[:error] = 'Invalid email/password combination.'
-        render 'new'
+        flash[:warning] = "You need to activate your account before you can log in. Check your email for the activation link."
+        redirect_to login_path
       end
+    else
+      flash.now[:error] = 'Invalid email/password combination.'
+      render 'new'
     end
   end
 
