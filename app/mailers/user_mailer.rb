70f3a30f3e9bde2884e3df85ad575e23ba9ee79e
Alexander Maru <i@alexandermaru.com>
2020-03-30 07:22:06 -0400

    Edit user mailer info

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
   def account_activation(user)
     @user = user
-    mail to: user.email, subject: "Kidanemihret EOTC: Account activation"
+    mail to: user.email, subject: "Kidanemihret - Account Activation"
   end
 
   def password_reset(user)
     @user = user
-    mail to: user.email, subject: "Kidanemihret EOTC: Password reset"
+    mail to: user.email, subject: "Kidanemihret - Password Reset"
   end
 end
